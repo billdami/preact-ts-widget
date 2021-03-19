52c7adf598cc3c255b55bdf8a567b78dff75b6e5
Bill Dami <billdami@gmail.com>
2021-03-19 15:07:22 -0400

    prep for custom css bundling/injection logic

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,20 @@ import habitat from 'preact-habitat';
 
 import Widget from './components/Widget';
 
+const WIDGET_NAME = 'preact-ts-widget';
+
 const _habitat = habitat(Widget);
 
+// TODO the css styles string global will be prepended to the bundle at build time
+if (window[`${WIDGET_NAME}-css`]) {
+    const head = document.head || document.getElementsByTagName('head')[0];
+    const style = document.createElement('style');
+    head.appendChild(style);
+    style.type = 'text/css';
+    style.appendChild(document.createTextNode(window[`${WIDGET_NAME}-css`]));
+}
+
 _habitat.render({
-    selector: '[data-widget-host="preact-ts-widget"]',
+    selector: `[data-widget-host="${WIDGET_NAME}"]`,
     clean: true,
 });
